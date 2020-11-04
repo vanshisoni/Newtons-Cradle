9e24b97ab661893749026d89bee838da79fb453b
vanshisoni <71668129+vanshisoni@users.noreply.github.com>
2020-11-04 16:00:52 -0500

    Add files via upload

---
+++ constraint.js
@@ -0,0 +1,18 @@
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB
+            
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    
+    display(){
+        line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,this.pointB.x,this.pointB.y);
+    }
+    
+}
